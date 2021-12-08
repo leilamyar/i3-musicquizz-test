6d59004f8dc9d08e39202e482e649878ed42be5a
leilamyar <leilamyar.dev@gmail.com>
2021-12-08 18:55:43 +0100

    feat(answers) : random correct and wrongs WIP

--- script.js
+++ script.js
@@ -3,30 +3,28 @@ document.addEventListener("DOMContentLoaded", function(){
   // ===================================================
   // =============== ELEM from the DOM =================
   // ===================================================
+
   const _startGameBtn = document.getElementById('start-game');
   const _lyrics = document.getElementById('lyrics');
   const _answerContainer = document.getElementById('answer-container');
-  const _a = document.querySelector('input[id="a"]');
-  const _b = document.querySelector('input[id="b"]');
-  const _c = document.querySelector('input[id="c"]');
-  const _d = document.querySelector('input[id="d"]');
   const _radioInputs = document.querySelectorAll('input[type="radio"]');
-  _radioInputs.forEach((ri) => ri.checked = false );
-
+  
   // ===================================================
   // ================= GAME STATE ======================
   // ===================================================
-
+  
   var scoreState = 0;
   var answerChoices = 0;
-
+  
+  // Re init radio input values, because are kept in cache
+  _radioInputs.forEach((ri) => ri.checked = false );
 
   // ===================================================
   // =============== GAME FUNCTIONS ====================
   // ===================================================
 
   // Function listenToAnswers
-  // Getting all checkboxes, and listen to whenever there're checked
+  // Getting all radio inputs, and listen to whenever there're checked
   const listenToAnswers = () => {
     document.querySelectorAll("input")
         .forEach((radio) => {
@@ -38,26 +36,6 @@ document.addEventListener("DOMContentLoaded", function(){
           });
         });
   }
-  
-  // Function generateWrongAnswers
-  // From the whole lyrics text of a song,
-  // randomly take 3 sentences (to populate wrong answers text)
-
-  const generateWrongAnswers = (tokens) => {
-
-    // let randTokenId = Math.floor(Math.random() * (tokens.length - 1) + 1);
-    const randSentences = { a: '', b: '', c: '' };
-
-    if (condition) {
-      
-    } else {
-      
-    }
-
-    randSentences.push(tokens[id]);
- 
-    return randSentences;
-  };
 
   const makeAnswerChoices = (correctText, tokenizedLyrics) => {
     
@@ -66,29 +44,69 @@ document.addEventListener("DOMContentLoaded", function(){
 
     console.log("rand ids ::", randomIds);
 
-    const b = tokenizedLyrics[randomIds[0]];
-    const c = tokenizedLyrics[randomIds[1]];
-    const d = tokenizedLyrics[randomIds[2]];
-
+    const wrongSentences = [
+                              tokenizedLyrics[randomIds[0]], // ex: 'We are legends'
+                              tokenizedLyrics[randomIds[1]], // ex: 'Everything you want's a dream away'
+                              tokenizedLyrics[randomIds[2]], // ex: 'That's what she told him!'
+                            ];
    
-    console.log("b", b);
-    console.log("c:::", c);
-    console.log("d :::", d);
-    // TODO: random correct letter
-    // const randomCorrect = 'b';
+    console.log("wrongSentences :::", wrongSentences);
+    
+    // 3 because we need only 3 Wrong answers for the quizz
+    // const answers = { 1: '', 2: '', 3: '', 4: '' };
+    // const answers = { a: '', b: '', c: '', d: '' };
+    const answers = [ { letter: 'a', answerText: '', isCorrect: false }, { letter: 'b', answerText: '', isCorrect: false }, { letter: 'c', answerText: '', isCorrect: false }, { letter: 'd', answerText: '', isCorrect: false } ];
+    // const answers = [ a, b, c, d ];
+    // const wrongSentences = [ { a: 0 } , {  b: 0 }, {c: 0 }, {d: 0 } ];
+    // const answers = { a: '', b: '', c: '', d: '' };
+    // const answers = { 1: 'a', 2: 'b', 3: 'c', 4: 'd' };
+    const randAnswerIds = makeRandomUniqueIds(4, 4);
+    // ex:  [ 2, 1, 3, 4 ]
+    // So in ex, randAnswerIds[0] is equal to 2
+    // So in ex, randAnswerIds[1] is equal to 1
+    // So in ex, randAnswerIds[2] is equal to 3
+    // So in ex, randAnswerIds[3] is equal to 4
 
-    _a.value = correctText;
-    _a.setAttribute('data-is-correct', true);
+    console.log("randAnswerIds :::", randAnswerIds);
+    const _a = document.querySelector('label[for="a"]');
+    const _b = document.querySelector('label[for="b"]');
+    const _c = document.querySelector('label[for="c"]');
+    const _d = document.querySelector('label[for="d"]');
 
-    _b.value = b;
-    _b.setAttribute('data-is-correct', false);
 
-    _c.value = c;
-    _c.setAttribute('data-is-correct', false);
+    console.log("answers :::", answers[1]);
+    // console.log("answers :::", answers);
 
-    _d.value = d;
-    _d.setAttribute('data-is-correct', false);
+    // Setting all answers :
+    // WRONG
+    answers[randAnswerIds[0]].answerText = wrongSentences[0];
+    answers[randAnswerIds[1]].answerText = wrongSentences[1];
+    answers[randAnswerIds[2]].answerText = wrongSentences[2];
+    // CORRECT
+    answers[randAnswerIds[3]].answerText = correctText;
+    answers[randAnswerIds[3]].isCorrect = true;
+    
+  
+    // answers.forEach(({ letter, answerText, isCorrect }) => {
+    //   // const _a = document.querySelector('label[for="a"]');
+    //   const _label = document.querySelector(`label[for="${letter}"]`);
+    //   _label.innerText = answerText;
+    //   _label.setAttribute('data-is-correct', isCorrect);
+    // });
+
+    // _a.innerText = answers.a;
+    // _a.setAttribute('data-is-correct', true);
+
+    // _b.value = choice1;
+    // _b.setAttribute('data-is-correct', false);
+
+    // _c.value = choice2;
+    // _c.setAttribute('data-is-correct', false);
+
+    // _d.value = choice3;
+    // _d.setAttribute('data-is-correct', false);
   };
+
   // Function Start Game
   // Treat lyrics & create correct & wrong answers
   const initGame = (song) => {
